f5edea8a12da51c70d6b51235767cdb53371cc37
ptjobs <ptjobs@protonmail.com>
2020-03-23 20:28:52 +0800

    图片库管理

--- includes/cls_image.php
+++ includes/cls_image.php
@@ -845,12 +845,18 @@ class cls_image
      */
     public function download_image($url, $path = '')
     {
+        if (!make_dir($path))
+        {
+            return false;
+        }
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
         $file = curl_exec($ch);
         curl_close($ch);
+
         $filename = pathinfo($url, PATHINFO_BASENAME);
         $resource = fopen($path . $filename, 'a');
         fwrite($resource, $file);
